0788f2f57e2d3afa8e28f12e230696514267e503
buentellopc <buentellopc@gmail.com>
2023-01-11 10:43:59 -0600

    equals defined vs not defined

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,23 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        Notepad basics = new Notepad("Equals basics", 2000);
-        Notepad advanced = new Notepad("Equals advanced", 2001);
+        ArrayList<Book> books = new ArrayList<>();
 
-        System.out.println(basics.equals(basics));
-        System.out.println(basics.equals(advanced));
-        System.out.println(basics.equals(new Notepad("Equals basics", 2000)));
-        System.out.println(basics.equals(new Notepad("Equals basics", 2001)));
+        Book bookObject = new Book("Book Object", 2000, "...");
+        books.add(bookObject);
+
+        if (books.contains(bookObject)) {
+            System.out.println("Book Object was found.");
+        }
+
+        bookObject = new Book("Book Object", 2000, "...");
+
+        if (!books.contains(bookObject)) {
+            System.out.println("Book Object was not found.");
+        }
     }
 }
